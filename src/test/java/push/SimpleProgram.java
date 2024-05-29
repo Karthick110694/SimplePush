dafe5fc496a07a75a53c57aa4eb79400d9c7defd
Karthick <karthickvel809@gmail.com>
2024-05-29 19:02:08 +0530

    New Commit to Git

---
+++ src/test/java/push/SimpleProgram.java
@@ -0,0 +1,12 @@
+package push;
+
+public class SimpleProgram {
+
+	public static void main(String[] args) {
+		System.out.println(add(10,1));
+	}
+	public static int add(int a, int b) {
+		return a+b;
+	}
+
+}
